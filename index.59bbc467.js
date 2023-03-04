c577e26b6dbfea9e37dece97a3ef169417e2b49e
mashapopov <111521332+mashapopov@users.noreply.github.com>
2023-03-04 20:24:13 +0000

    Deploying to gh-pages from @ mashapopov/goit-js-hw-12@d06ccdf2d6cc2714135fabdeac21dabcf0794634 🚀

---
+++ index.59bbc467.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".search"),c=document.querySelector(".search__img"),o=document.querySelector(".search__btn-ds");c.addEventListener("click",(function(c){c.stopPropagation(),e.classList.add("search-activ"),console.log("open")})),o.addEventListener("click",(function(){e.classList.remove("search-activ"),console.log("close")}))}();
+//# sourceMappingURL=index.59bbc467.js.map
---
+++ index.9386bc8d.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".search"),c=document.querySelector(".search__img"),o=document.querySelector(".search__btn-ds");c.addEventListener("click",(function(c){c.stopPropagation(),e.classList.add("search-activ"),console.log("open")})),o.addEventListener("click",(function(){e.classList.remove("search-activ"),console.log("close")}));
+//# sourceMappingURL=index.9386bc8d.js.map
